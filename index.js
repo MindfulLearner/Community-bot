ae84ef4503d668508cff7c07ed02321489f90842
Granze <granzebru@gmail.com>
2025-10-22 16:36:45 +0200

    implement commands deploy

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 // Require the necessary discord.js classes
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags, REST } = require('discord.js');
 const { token } = process.env.DISCORD_TOKEN;
+const { clientId } = process.env.CLIENT_ID;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -76,4 +77,44 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
 	console.log(`Health check server running on port ${PORT}`);
+});
+
+async function deployCommands() {
+  try {
+    const commands = [];
+    const foldersPath = path.join(__dirname, 'commands');
+    const commandFolders = fs.readdirSync(foldersPath);
+
+    for (const folder of commandFolders) {
+      const commandsPath = path.join(foldersPath, folder);
+      const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+      for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+
+        if ('data' in command && 'execute' in command) {
+          commands.push(command.data.toJSON());
+        } else {
+          console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+      }
+    }
+
+    const rest = new REST().setToken(token);
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    const data = await rest.put(
+      Routes.applicationCommands(clientId),
+      { body: commands },
+    );
+
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+deployCommands().then(() => {
+  client.login(token);
 });
